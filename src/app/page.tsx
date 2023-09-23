64d1c1ad6946e0295a68180484cd900b368c225a
Claire <claire@getcontrol.co>
2023-09-23 15:58:08 -0700

    unescaped apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image"
-
 export default function Home() {
   return (
     <main className="flex min-h-full overflow-auto flex-col items-center justify-between p-24">
       <div className="z-2 flex-col max-w-5xl w-full items-start justify-between font-mono text-sm lg:flex">
         <h2>Welcome to my Case Study </h2>
         <p>
-          Hi, I'm Claire, a full stack developer from Vancouver, BC, Canada.
-          Thank you for taking a look at my case study!
+          Hi, I&apos;m Claire, a full stack developer from Vancouver, BC,
+          Canada. Thank you for taking a look at my case study!
         </p>
         <br />
         <p>Take a look at the shop to add items to your cart</p>
